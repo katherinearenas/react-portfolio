143f1afefe07a9aa526aa7cc5426b1d38be9fe89
Katherine <89894177+katherinearenas@users.noreply.github.com>
2025-01-10 14:45:09 -0600

    added css to about page

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,8 +1,24 @@
-export default function About() {
-    return(
-        <div class="card-body">
-        <h3 class="card-text">Hi, I'm Katherine, a former attorney, turned artist and web developer. I enjoy building web applications and . I'm really enjoying working with The MERN stack, especially Express, React, and Node.js.</h3>
-        <a href="https://github.com/katherinearenas" class="btn btn-primary">Here's a link to my Github</a>
+import React from "react";
+import "./style/about.css";
+
+function About() {
+  return (
+    <div className="about-container">
+      <h1>About Me</h1>
+      <div className="about-content">
+        <p>
+          Hi, I’m Katherine Arenas, a full-stack web developer with a passion for creating dynamic and responsive web applications. With a certification from Northwestern University in Fullstack Web Development, I specialize in JavaScript, React, Node.js, MongoDB, and GraphQL and am always interested in learning more about programming tools and languages. My journey into tech was fueled by my love for problem-solving and a desire to bring ideas to life through code.
+        </p>
+        <p>
+          Before I became a software developer, I earned my Juris Doctorate in Law  and was a practicing Attorney in Chicago Illinois. As an attorney, I honed skills in research, writing, and collaboration. These experiences have enriched my approach to software development, allowing me to bring a unique perspective to exciting new projects.
+        </p>
+        <p>
+          When I’m not coding, you can find me exploring my creative side by crafting music, designing puzzle games, or diving into stories that spark the imagination. I’m always excited to learn new technologies, collaborate with like-minded individuals, and build solutions that make a difference.
+        </p>
+        <p>
+          Let’s connect and create something amazing together!
+        </p>
+      </div>
     </div>
-    )  
+  );
 }
